ed5f95e0090175f424372ef5828c9c3cb4fdc2e4
João Vitor Mendonça <91424123+jvittortrikx1997@users.noreply.github.com>
2023-03-14 12:37:21 -0300

    docs

--- app/Http/Controllers/DocController.php
+++ app/Http/Controllers/DocController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Doc;
 
 class DocController extends Controller
 {
-    //
+    public function getAll(){
+        $array = ['error' => ''];
+
+        $user = auth()->user();
+
+        $docs = Doc::all();
+
+        $array['list'] = $docs;
+        foreach($docs as $docKey => $docValue ){
+            $docs[$docKey]['fileurl'] = asset('storage/'.$docValue['fileurl']);
+        }
+
+        return $array;
+    }
 }
