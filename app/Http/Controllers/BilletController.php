8abb7017ae791a8c8f4f77004da8e67fbb618429
João Vitor Mendonça <91424123+jvittortrikx1997@users.noreply.github.com>
2023-03-14 20:25:34 -0300

    billets

--- app/Http/Controllers/BilletController.php
+++ app/Http/Controllers/BilletController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Billet;
+use App\Models\Unit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class BilletController extends Controller
 {
-    //
+    public function getAll(Request $request){
+        $array = ['error' => ''];
+
+        $user = auth()->user();
+
+        $property = $request->input('property');
+        $unit = Unit::where('id', $property)->where('id_owner', $user['id'])->count();
+
+        if($property){
+            if($unit > 0){
+                $billets = Billet::where('id_unit', $property)->get();
+
+                foreach($billets as $billetKey => $billetValue){
+                    $billets[$billetKey]['fileurl'] = asset('storage/'.$billetValue['fileurl']);
+                }
+                $array['list'] = $billets;
+            }else{
+                $array['error'] = 'A unidade informada não é sua.';
+            }
+        }else{
+            $array['error'] = 'A propriedade é obrigatória.';
+        }
+        return $array;
+    }
 }
