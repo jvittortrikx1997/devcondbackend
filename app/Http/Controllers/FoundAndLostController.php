16a2f7689c1051af99e17a219d02d0822f3fa496
João Vitor Mendonça <91424123+jvittortrikx1997@users.noreply.github.com>
2023-03-15 20:12:28 -0300

    foundandlost

--- app/Http/Controllers/FoundAndLostController.php
+++ app/Http/Controllers/FoundAndLostController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\FoundAndLost;
 use Illuminate\Http\Request;
 
 class FoundAndLostController extends Controller
 {
-    //
+    public function getAll(){
+        $array = ['error' => ''];
+        $user = auth()->user();
+
+        $lost = FoundAndLost::where('status', 'LOST')
+        ->orderBy('datecreated', 'DESC')
+        ->orderBy('id', 'DESC')
+        ->get();
+
+        $recovered = FoundAndLost::where('status', 'RECOVERED')
+        ->orderBy('datecreated', 'DESC')
+        ->orderBy('id', 'DESC')
+        ->get();
+
+        foreach($lost as $lostKey => $lostValue){
+            $lost[$lostKey]['datecreated'] = date('d/m/Y', strtotime($lostValue['datecreated']));
+            $lost[$lostKey]['photo'] = asset('storage/'.$lostValue['photo']);
+        }
+
+        foreach($recovered as $recoveredKey => $recoveredValue){
+            $recovered[$recoveredKey]['datecreated'] = date('d/m/Y', strtotime($recoveredValue['datecreated']));
+            $recovered[$recoveredKey]['photo'] = asset('storage/'.$recoveredValue['photo']);
+        }
+
+        $array['lost'] = $lost;
+        $array['recovered'] = $recovered;
+
+
+        return $array;
+    }
 }
