a24025b35172fcb77e71dfef903161c1499127d2
João Vitor Mendonça <91424123+jvittortrikx1997@users.noreply.github.com>
2023-03-16 18:29:15 -0300

    getReservations

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -3,8 +3,60 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Area;
 
 class ReservationController extends Controller
 {
-    //
+    public function getReservation(){
+        $array = ['error' => '', 'list' => []];
+        $daysHelper = ['Dom', 'Seg', 'Ter', 'Quar', 'Quin', 'Sex', 'Sab'];
+
+        $areas = Area::where('allowed', 1)->get();
+
+        foreach($areas as $area){
+            $dayList = explode(',', $area['days']);
+            $dayGroups = [];
+
+            $lastDay = intval(current($dayList));
+            $dayGroups[] = $daysHelper[$lastDay];
+            array_shift($dayList);
+
+            foreach($dayList as $day){
+                if(intval($day) != $lastDay+1){
+                    $dayGroups[] = $daysHelper[$lastDay];
+                    $dayGroups[] = $daysHelper[$day];
+                }
+                $lastDay = intval($day);
+            }
+            $dayGroups[] = $daysHelper[end($dayList)];
+            $dates = '';
+            $close = 0;
+            foreach($dayGroups as $group){
+                if($close == 0){
+                    $dates .=$group;
+                }else{
+                    $dates .= '-'.$group.',';
+                }
+                $close = 1 - $close;
+            }
+
+            $dates = explode(',', $dates);
+            array_pop($dates);
+
+            $startTime = date('H:i', strtotime($area['start_time']));
+            $endTime = date('H:i', strtotime($area['end_time']));
+
+            foreach($dates as $dKey => $dValue){
+                $dates[$dKey] .= ' '.$startTime.' às '.$endTime;
+            }
+
+            $array['list'][] = [
+                'id' => $area['id'],
+                'cover' => asset('storage/'.$area['cover']),
+                'tile' => $area['title'],
+                'dates' => $dates
+            ];
+        }
+        return $array;
+    }
 }
