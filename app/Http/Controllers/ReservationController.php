b58fa859c3b3eca103039c705933b59db0894290
João Vitor Mendonça <91424123+jvittortrikx1997@users.noreply.github.com>
2023-03-17 12:55:23 -0300

    Reservation1

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Area;
+use App\Models\AreaDisabledDay;
+use App\Models\Reservation;
+use App\Models\Unit;
+use Illuminate\Support\Facades\Validator;
 
 class ReservationController extends Controller
 {
@@ -57,6 +61,123 @@ class ReservationController extends Controller
                 'dates' => $dates
             ];
         }
+        return $array;
+    }
+
+    public function setReservation($id, Request $request){
+        $array = ['error' => ''];
+
+        $validator = Validator::make($request->all(), [
+            'date' => 'required|date_format:Y-m-d',
+            'time' => 'required|date_format:H:i:s',
+            'property' => 'required'
+        ]);
+
+        if(!$validator->fails()){
+            $date = $request->input('date');
+            $time = $request->input('time');
+            $property = $request->input('property');
+
+            $unit = Unit::find($property);
+            $area = Area::find($id);
+
+            if($unit && $area){
+                $can = true;
+                $weekDay = date('w', strtotime($date));
+                $allowedDays = explode(',', $area['days']);
+                if(!in_array($weekDay, $allowedDays)){
+                    $can = false;
+                }else{
+                    $start = strtotime($area['start_time']);
+                    $end = strtotime('-1 hour', strtotime($area['end_time']));
+                    $revtime = strtotime($time);
+                    if($revtime < $start || $revtime > $end){
+                        $can = false;
+                    }
+                }
+                $existingDisabledDay = AreaDisabledDay::where('id_area', $id)
+                ->where('day', $date)
+                ->count();
+                if($existingDisabledDay > 0){
+                    $can = false;
+                }
+                $existingReservation = Reservation::where('id_area', $id)
+                ->where('reservation_date', $date.' '.$time)
+                ->count();
+                if($existingReservation){
+                    $can = false;
+                }
+
+                if($can == true){
+                    $reservation = new Reservation();
+                    $reservation->id_unit = $property;
+                    $reservation->id_area = $id;
+                    $reservation->reservation_date = $date.' '.$time;
+                    $reservation->save();
+                }else{
+                    $array['error'] = 'Reserva não permitida nesse período.';
+                    return $array;
+                }
+            }else{
+                $array['error'] = 'Dados incorretos';
+                return $array;
+            }
+        }else{
+            $array['error'] = $validator->errors()->first();
+            return $array;
+        }
+        return $array;
+    }
+
+    public function getDisabledDates($id){
+        $array = ['error' => '', 'list' => []];
+        $area = Area::find($id);
+
+        if($area){
+            $disabledDays = AreaDisabledDay::where('id_area', $id)->get();
+            foreach($disabledDays as $disabledDay){
+                $array['list'][] = $disabledDay['day'];
+            }
+
+            $allowedDays = explode(',', $area['days']);
+            $offDays = [];
+            for($q=0;$q<7;$q++){
+                if(!in_array($q, $allowedDays)){
+                    $offDays[] = $q;
+                }
+            }
+
+            $start = time();
+            $end = strtotime('+3 month');
+            $current = $start;
+            /*$keep = true;
+            while($keep){
+                if($current < $end){
+                    $wd = date('w', $current);
+                    if(in_array($wd, $offDays)){
+                        $array['list'][] = date('Y-m-d', $current);
+                    }
+                    $current = strtotime('+1 day', $current);
+                }else{
+                    $keep = false;
+                }
+            }*/
+
+            for(
+                $current = $start;
+                $current < $end;
+                $current = strtotime('+1 day', $current)
+            ){
+                $wd = date('w', $current);
+                    if(in_array($wd, $offDays)){
+                        $array['list'][] = date('Y-m-d', $current);
+                    }
+            }
+        }else{
+            $array['error'] = 'A area informada é inválida';
+            return $array;
+        }
+
         return $array;
     }
 }
